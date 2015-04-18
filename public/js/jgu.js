e3728e81f67d551b84d612154165c607b0415cc1
jgu <jeffwgu@gmail.com>
2015-04-18 09:38:29 -0600

    changed work page

--- public/js/jgu.js
+++ public/js/jgu.js
@@ -65,10 +65,10 @@ var svg = d3.select("#donut").append("svg")
 .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 data = [
-  {"lang": "Ruby", "score": 9},
-  {"lang": "Javascript", "score": 6},
+  {"lang": "Ruby", "score": 8},
+  {"lang": "Javascript", "score": 5},
   {"lang": "d3", "score": 2},
-  {"lang": "Go", "score": 2},
+  {"lang": "Go", "score": 3},
   {"lang": "Android", "score": 3}
 ]
 
